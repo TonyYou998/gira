74d2e99495a114926e750f27d2aefd6c722f234c
TonyYou998 <19520931@gm.uit.edu.com>
2021-07-31 14:40:56 +0700

    init

---
+++ src/main/java/cybersoft/javabackend/java12/gira/welcome/WelcomeController.java
@@ -0,0 +1,13 @@
+package cybersoft.javabackend.java12.gira.welcome;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+@Controller
+public class WelcomeController {
+	@GetMapping("")
+	@ResponseBody
+	public String welcome() {
+		return "welcome";
+	}
+}
